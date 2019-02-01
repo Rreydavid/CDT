2d6468e24a068a463c994eaea9892723e4e9fbdc
David Mercado <dme236@g.uky.edu>
2019-02-01 12:37:06 -0500

    Start of iScout!

---
+++ iScout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iScout
+//
+//  Created by David Mercado on 2/1/19.
+//  Copyright © 2019 CDT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
