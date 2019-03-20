ac7ef221f35f4881da47cecec3c77a95d3426d0a
David Mercado <dme236@g.uky.edu>
2019-03-20 12:03:35 -0400

    started working on the routing display for the gps data

--- iScout/ViewControllers/MapViewController.swift
+++ iScout/ViewControllers/MapViewController.swift
@@ -15,6 +15,9 @@ class MapViewController: UIViewController {
     
     @IBOutlet weak var MapView: MKMapView!
     
+    //var park = Park(filename: "Places")
+    //var selectedOptions : [MapOptionsType] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -24,9 +27,44 @@ class MapViewController: UIViewController {
         MapView.mapType = MKMapType.init(rawValue: UInt(sender.selectedSegmentIndex)) ?? .standard
     }
     
+    /*func addRoute() {
+        guard let points = Park.plist("Places") as? [String] else { return }
+        
+        let cgPoints = points.map { NSCoder.cgPoint(for: $0) }
+        let coords = cgPoints.map { CLLocationCoordinate2DMake(CLLocationDegrees($0.x), CLLocationDegrees($0.y)) }
+        let myPolyline = MKPolyline(coordinates: coords, count: coords.count)
+        
+        MapView.addOverlay(myPolyline)
+    }*/
+    
+    // MARK: Helper methods
+    func loadSelectedOptions() {
+        MapView.removeAnnotations(MapView.annotations)
+        MapView.removeOverlays(MapView.overlays)
+        
+        /*for option in selectedOptions {
+            switch (option) {
+            case .mapOverlay:
+                self.addOverlay()
+            case .mapPins:
+                self.addAttractionPins()
+            case .mapRoute:
+                self.addRoute()
+            case .mapBoundary:
+                self.addBoundary()
+            case .mapCharacterLocation:
+                self.addCharacterLocation()
+            }
+        }*/
+    }
 }
 
 // MARK: - MKMapViewDelegate
 extension MapViewController: MKMapViewDelegate {
     
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        
+        
+        return MKOverlayRenderer()
+    }
 }
